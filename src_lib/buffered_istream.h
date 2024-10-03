c9284110e6eb227df0d39711cfaa40c62af85c45
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2024-10-03 00:01:36 -0700

    worked on custom streambuf

--- src_lib/buffered_istream.h
+++ src_lib/buffered_istream.h
@@ -9,12 +9,10 @@ class buffered : public std::streambuf
 {
     std::streambuf *m_streambuf;
     std::string m_buffer;
-    int m_next_index;
 
 public:
     buffered(
-        std::streambuf *a_streambuf) : m_streambuf(a_streambuf),
-                                       m_next_index(0)
+        std::streambuf *a_streambuf) : m_streambuf(a_streambuf)
     {
         setg(nullptr, nullptr, nullptr);
     }
@@ -23,9 +21,72 @@ public:
     {
     }
 
+protected:
+
     virtual int_type underflow() override
     {
-        if (m_next_index == m_buffer.size())
+        
+        return m_buffer[m_next_index];
+    }
+
+    virtual std::streampos seekoff(
+        std::streamoff a_offset,
+        std::ios_base::seekdir a_way,
+        std::ios_base::openmode a_which = std::ios_base::in
+    ) override
+    {
+        // handle which
+        
+        if (!(a_which & std::ios::in))
+            // indicate that this streambuf only works with
+            //     input data.
+            return std::streampos(std::streamoff(-1));
+
+        // handle way
+
+        std::streamoff l_new_position;
+
+        switch (a_way)
+        {
+            case std::ios_base::beg:
+            {
+                l_new_position = a_offset;
+                break;
+            }
+            case std::ios_base::cur:
+            {
+                l_new_position = m_next_index + a_offset;
+                break;
+            }
+            case std::ios_base::end:
+            {
+                l_new_position = m_buffer.size() + a_offset;
+                break;
+            }
+            default:
+            {
+                return std::streampos(std::streamoff(-1));
+            }
+        }
+
+        // return error if negative index
+
+        if (l_new_position < 0)
+            return std::streampos(std::streamoff(-1));
+
+        if (m_buffer.size() < static_cast<size_t>(l_new_position))
+        {
+
+        }
+        
+        
+    }
+
+    int load_more_data(int a_n)
+    {
+        int i = 0;
+
+        for (; i < a_n; ++i)
         {
             // reached eof of internal buffer
 
@@ -42,12 +103,16 @@ public:
 
             // set information pertaining to readable buffer space
             setg(m_buffer.data(),
-                 m_buffer.data() + m_next_index,
-                 m_buffer.data() + m_buffer.size());
+                    m_buffer.data() + m_next_index,
+                    m_buffer.data() + m_buffer.size());
+        
+
         }
 
-        return m_buffer[m_next_index];
+        
+
     }
+    
 };
 
 #endif
