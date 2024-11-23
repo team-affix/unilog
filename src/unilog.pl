b8fc58d200331c85148c4c28c8ba13394837d4fc
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2024-11-22 18:26:49 -0800

    Rearranged DStack and BStack such that BStack is the left-most argument. This is helpful visually since now, as your eyes traverse from right to left, across both DStack and BStack in totality, you enter more inner scopes. Thus the left-most entry in the left-most argument (BStack) is the innermost scope.

--- src/unilog.pl
+++ src/unilog.pl
@@ -35,104 +35,67 @@ query(Guide, Theorem) :-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% scope handling
 
-query(DStack, BStack, [bout, S, NextGuide], [claims, S, Internal]) :-
+query(BStack, DStack, [bout, S, NextGuide], [claims, S, Internal]) :-
     %bscope(ScopedSExpr, [S], Internal),
-    query(DStack, [S|BStack], NextGuide, Internal).
+    query([S|BStack], DStack,  NextGuide, Internal).
 
-query(DStack, [S|BStack], [bin, S, NextGuide], Internal) :-
+query([S|BStack], DStack, [bin, S, NextGuide], Internal) :-
     %bscope(ScopedSExpr, [S], Internal),
-    query(DStack, BStack, NextGuide, [claims, S, Internal]).
+    query(BStack, DStack, NextGuide, [claims, S, Internal]).
 
-query(DStack, BStack, [dout, S, NextGuide], Theorem) :-
+query(BStack, DStack, [dout, S, NextGuide], Theorem) :-
     append(NewBStack, [S], BStack),
-    query([S|DStack], NewBStack, NextGuide, Theorem).
+    query(NewBStack, [S|DStack], NextGuide, Theorem).
 
-query([S|DStack], BStack, [din, S, NextGuide], Theorem) :-
+query(BStack, [S|DStack], [din, S, NextGuide], Theorem) :-
     append(BStack, [S], NewBStack),
-    query(DStack, NewBStack, NextGuide, Theorem).
+    query(NewBStack, DStack, NextGuide, Theorem).
 
 %%%%%%%%%%%%%%%%%%%%% less fundamental ROIs
 
-query(DStack, BStack, [bind, Target, NextGuide], Target) :-
-    query(DStack, BStack, NextGuide, Target).
+query(BStack, DStack, [bind, Target, NextGuide], Target) :-
+    query(BStack, DStack, NextGuide, Target).
 
-query(DStack, BStack, [sub, SubTarget, SubGuide, NextGuide], Target) :-
-    query(DStack, BStack, SubGuide, SubTarget),
-    query(DStack, BStack, NextGuide, Target).
+query(BStack, DStack, [sub, SubTarget, SubGuide, NextGuide], Target) :-
+    query(BStack, DStack, SubGuide, SubTarget),
+    query(BStack, DStack, NextGuide, Target).
 
-query(DStack, BStack, [cond, [[CondTheorem|CondGuide]|NextGuide] | CondsRest], Target) :-
-    query(DStack, BStack, CondGuide, CondTheorem),
+query(BStack, DStack, [cond, [[CondTheorem|CondGuide]|NextGuide] | CondsRest], Target) :-
+    query(BStack, DStack, CondGuide, CondTheorem),
     !, % if condition goal succeeds, do NOT try any other branches
-    query(DStack, BStack, NextGuide, Target)
+    query(BStack, DStack, NextGuide, Target)
     ;
-    query(DStack, BStack, [cond | CondsRest], Target).
+    query(BStack, DStack, [cond | CondsRest], Target).
 
-query(DStack, BStack, [eval], [eval, Guide]) :-
-    query(DStack, BStack, Guide, _).
+query(BStack, DStack, [eval], [eval, Guide]) :-
+    query(BStack, DStack, Guide, _).
 
-query(DStack, BStack, [fail], [fail, Guide]) :-
-    \+ query(DStack, BStack, Guide, _).
+query(BStack, DStack, [fail], [fail, Guide]) :-
+    \+ query(BStack, DStack, Guide, _).
 
 % logic ROI
 
-query(DStack, BStack, [mp, ImpGuide, JusGuide], Y) :-
-    query(DStack, BStack, ImpGuide, [if, Y, X]),
-    query(DStack, BStack, JusGuide, X).
+query(BStack, DStack, [mp, ImpGuide, JusGuide], Y) :-
+    query(BStack, DStack, ImpGuide, [if, Y, X]),
+    query(BStack, DStack, JusGuide, X).
 
 % terminal ROI
 
-query(DStack, [], [theorem, AxiomTag], Theorem) :-
+query([], DStack, [theorem, AxiomTag], Theorem) :-
     theorem(DStack, AxiomTag, Theorem).
 
-query(DStack, BStack, [guide, GuideTag | GuideArgs], Theorem) :-
-    guide(DStack, GuideTag, GuideArgs, Redirect),
-    query(DStack, BStack, Redirect, Theorem).
+query(BStack, DStack, [guide, GuideTag], Theorem) :-
+    guide(DStack, GuideTag, Redirect),
+    query(BStack, DStack, Redirect, Theorem).
 
 :- dynamic theorem/3.
 :- dynamic guide/3.
 
-%:-
-%    axiom([], a0, [awesome, jake]),
-%    axiom([], a1, [if, y, x]),
-%    axiom([], a2, x),
-%    axiom([], a3, [believe, m1, x]),
-%    axiom([], a4, [believe, m1, [if, y, x]]),
-%    axiom([], a5,
-%        [believe, m1,
-%            [if,
-%                [believe, m2, b],
-%                a
-%            ]
-%        ]),
-%    axiom([], a6, [believe, m1, a]),
-%    axiom([], a7,
-%        [believe, m1,
-%            [believe, m2,
-%                [if,
-%                    c,
-%                    b
-%                ]
-%            ]
-%        ]),
-%    axiom([m3], a0, [believe, m1, [if, y, x]]),
-%    axiom([m3], a1, [believe, m1, x])
-%    .
-%
-%:-
-%    \+ query([mp, a4, a3], _),
-%    query([bout, m1, [mp, [bin, m1, a4], [bin, m1, a3]]], _),
-%    query([bout, m1, [mp, [bin, m1, a4], [bin, m1, a3]]], R2),
-%        unify(R2, [believe, m1, y]),
-%    query([bout, m1, [bout, m2, [mp, [bin, m2, [bin, m1, a7]], [bin, m2, [mp, [bin, m1, a5], [bin, m1, a6]]]]]], R3),
-%        unify(R3, [believe, m1, [believe, m2, c]]),
-%    query([bout, m3, [gout, m3, [bout, m1, [mp, [bin, m1, a0], [bin, m1, a1]]]]], R4),
-%        unify(R4,
-%            [believe, m3,
-%                m3:[
-%                    believe, m1, y
-%                ]
-%            ]),
-%    query([bout, m3, [gout, m3, [bout, m1, [mp, [bin, m1, a0], [bin, m1, a1]]]]],
-%        [believe, m3, [believe, m3:m1, m3:y]]
-%        )
-%    .
+:-
+    decl_theorem([natalie, daniel], a0, [if, y, x]),
+    decl_theorem([natalie, daniel], a1, x),
+    query(
+        [bout, daniel, [bout, natalie,
+        [dout, daniel, [dout, natalie,
+            [mp, [theorem, a0], [theorem, a1]]
+        ]]]], _).
